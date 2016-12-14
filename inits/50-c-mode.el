07a31ed869eefd6be6579d3196aff143a47339b6
Yuichi Itami <itaooooo@gmail.com>
2016-12-14 21:06:08 +0900

    C++関連の設定が上手く機能していなかった部分を一部修正.

--- inits/50-c-mode.el
+++ inits/50-c-mode.el
@@ -9,7 +9,7 @@
   (c-set-style "stroustrup")
   (show-paren-mode t)
   (setq tab-width 4)
-  (setq c-basic-offset 4)
+  (setq-default c-basic-offset 4)
   (setq indent-tabs-mode nil)
   )
 
@@ -18,15 +18,27 @@
   "C only original mode setup function."
   )
 
-;; C++言語向け
-(defun my-c++-mode ()
-  "C++ only original mode setup function."
-  (c-set-offset 'innamespace 0)     ;; namespace {}ではインデントしない.
-  (c-set-offset 'arglist-intro '+)  ;; 引数リストの引数の一行目.
-  (c-set-offset 'inher-intro '+)    ;;
-  (c-set-offset 'case-label '+)     ;; switch文内のcase/default label
-  )
+;; (defun my-c++-mode ()
+;;   "C++ only original mode setup function."
+;;   (c-set-offset 'innamespace 0)     ;; namespace {}ではインデントしない.
+;;   (c-set-offset 'arglist-intro '+)  ;; 引数リストの引数の一行目.
+;;   (c-set-offset 'inher-intro '+)    ;;
+;;   (c-set-offset 'case-label '+)     ;; switch文内のcase/default label
+;;   )
 
+;; C++言語向け
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (c-set-style "stroustrup")
+            (show-paren-mode t)
+            (setq tab-width 4)
+            (setq-default c-basic-offset 4)
+            (setq indent-tabs-mode nil)
+            (c-set-offset 'innamespace 0)     ;; namespace {}ではインデントしない.
+            (c-set-offset 'arglist-intro '+)  ;; 引数リストの引数の一行目.
+            (c-set-offset 'inher-intro '+)    ;;
+            (c-set-offset 'case-label '+)     ;; switch文内のcase/default label
+            ))
 
 ;;=======================================================================
 ;; c-mode-hook, c++-mode-hookに定義した関数を渡す.
@@ -39,11 +51,10 @@
 (add-hook 'c-mode-hook 'my-c-mode)
 
 ;; (add-hook 'c++-mode-hook
-;;           '(lambda()
-;;              '('my-c-common-mode
-;;                'my-c++-mode)))
-(add-hook 'c++-mode-hook 'my-c-common-mode)
-(add-hook 'c++-mode-hook 'my-c++-mode)
+;;           '(lambda ()
+;;              '('(my-c-common-mode)
+;;                '(my-c++-mode))))
+;;(add-hook 'c++-mode-hook 'my-c-common-mode 'my-c++-mode)
 
 
 ;;=======================================================================
