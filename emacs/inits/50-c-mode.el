8b0786d0e571164649487a21009b6bd5cd0e0539
boiled_mag <itaooooo@gmail.com>
2017-10-09 18:24:03 +0900

    C++設定を見直し.

--- emacs/inits/50-c-mode.el
+++ emacs/inits/50-c-mode.el
@@ -1,71 +1,40 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-;;=======================================================================
-;; Coding Style
-;;=======================================================================
-;; C/C++共通
-(with-eval-after-load 'c++-mode
-  (setq-default c-basic-offset 4))
-
-;; C/C++共通
-(defun my-c-common-mode ()
-  "C, C++ original mode setup function."
-  (c-set-style "stroustrup")
-  (show-paren-mode t)
-  (setq tab-width 4)
-  (setq indent-tabs-mode nil)
-  )
-
-;; C言語向け
-(defun my-c-mode ()
-  "C only original mode setup function."
-  )
-
-;; (defun my-c++-mode ()
-;;   "C++ only original mode setup function."
-;;   (c-set-offset 'innamespace 0)     ;; namespace {}ではインデントしない.
-;;   (c-set-offset 'arglist-intro '+)  ;; 引数リストの引数の一行目.
-;;   (c-set-offset 'inher-intro '+)    ;;
-;;   (c-set-offset 'case-label '+)     ;; switch文内のcase/default label
-;;   )
-
-;; C++言語向け
+;;;
+;;; C++言語向け
+;;;
 (add-hook 'c++-mode-hook
           (lambda ()
             (c-set-style "stroustrup")
-            (show-paren-mode t)
             (setq tab-width 4)
+            (setq c-basic-offset 4)
             (setq indent-tabs-mode nil)
-            (c-set-offset 'innamespace 0)     ;; namespace {}ではインデントしない.
-            (c-set-offset 'arglist-intro '+)  ;; 引数リストの引数の一行目.
-            (c-set-offset 'inher-intro '+)    ;;
-            (c-set-offset 'case-label '+)     ;; switch文内のcase/default label
+            (setq c-hungry-delete-key t)     ; 複数の空白を一度に削除.
+            (c-set-offset 'innamespace 0)    ; namespace {}ではインデントしない.
+            (c-set-offset 'arglist-intro '+) ; 引数リストの引数の一行目.
+            (c-set-offset 'inher-intro '+)   ;
+            (c-set-offset 'case-label '+)    ; switch文内のcase/default label
             ))
 
-;;=======================================================================
-;; c-mode-hook, c++-mode-hookに定義した関数を渡す.
-;;=======================================================================
+
+;;;
+;;; C言語向け
+;;;
 ;; (add-hook 'c-mode-hook
-;;           '(lambda()
-;;              '('my-c-common-mode
-;;                'my-c-mode)))
-(add-hook 'c-mode-hook 'my-c-common-mode)
-(add-hook 'c-mode-hook 'my-c-mode)
-
-;; (add-hook 'c++-mode-hook
 ;;           '(lambda ()
-;;              '('(my-c-common-mode)
-;;                '(my-c++-mode))))
-;;(add-hook 'c++-mode-hook 'my-c-common-mode 'my-c++-mode)
+;;              (setq c-basic-offset 4)
+;;              (setq c-hungry-delete-key t)     ; 複数の空白を一度に削除.
+;;              ))
 
-
-;;=======================================================================
-;; helm-gtags-mode
-;;=======================================================================
+;;;
+;;; helm-gtags-mode
+;;;
 (add-hook 'c-mode-hook 'helm-gtags-mode)
 (add-hook 'c++-mode-hook 'helm-gtags-mode)
 
-;; highlight-symbol-mode
+;;;
+;;; highlight-symbol-mode
+;;;
 (add-hook 'c-mode-hook 'highlight-symbol-mode)
 (add-hook 'c++-mode-hook 'highlight-symbol-mode)
 
