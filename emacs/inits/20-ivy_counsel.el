ef2bb3365356d9db4f3046203c7c649eb89d8527
boiled-mag <itaooooo@gmail.com>
2018-05-06 21:42:39 +0900

    ・counsel-gtags.elの設定を追加.

--- emacs/inits/20-ivy_counsel.el
+++ emacs/inits/20-ivy_counsel.el
@@ -23,3 +23,17 @@
 (defvar counsel-find-file-ignore-regexp (regexp-opt '("./" "../")))
 
 
+;;
+;; counsel-gtags.el
+;;
+;; package.elにてcounsel-gtags.elをインストールしておく.
+(use-package counsel-gtags
+  :bind (("M-t" . 'counsel-gtags-find-definition)
+         ("M-r" . 'counsel-gtags-find-reference)
+         ("M-s" . 'counsel-gtags-find-symbol)
+         ("M-," . 'counsel-gtags-go-backward))
+         ("M-." . 'counsel-gtags-go-forward))
+  :config
+  (add-hook 'c-mode-hook 'counsel-gtags-mode)
+  (add-hook 'c++-mode-hook 'counsel-gtags-mode)
+  )
