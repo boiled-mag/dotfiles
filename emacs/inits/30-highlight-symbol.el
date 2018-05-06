445f70843310819e1dd8689409182bbfc99da2fe
boiled-mag <itaooooo@gmail.com>
2018-05-06 21:07:23 +0900

    highlight-symbol-modeの設定をuse-packageを使った書き方に変更. 同時に設定見直し.

--- emacs/inits/30-highlight-symbol.el
+++ emacs/inits/30-highlight-symbol.el
@@ -1,15 +1,24 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-(defun my/highlight-symbol-mode-config ()
-;  (set-variable highlight-symbol-colors '("DarkOrange" "DodgerBlue4" "DeepPink1"))
-  (set-variable highlight-symbol-idle-delay 0.5))
+(use-package highlight-symbol
+  :bind (("C-3"   . 'highlight-symbol-at-point)
+         ("M-C-3" . 'highlight-symbol-remove-all))
+  :config
+  (highlight-symbol-nav-mode t)
+  (setq highlight-symbol-idle-delay 1.0)  ; ハイライトするまでに1.0秒のディレイを入れる.
+  )
 
-(add-hook 'highlight-symbol-mode-hook 'my/highlight-symbol-mode-config)
 
-(when (require 'highlight-symbol nil t)
-  (with-eval-after-load 'highlight-symbol
-    (global-set-key (kbd "C-3") 'highlight-symbol-at-point)
-    (global-set-key (kbd "M-C-3") 'highlight-symbol-remove-all)
-;;    (set-variable highlight-nonselected-windows t)))
-    (setq highlight-nonselected-windows t)))
+;; use-packageに書き換え. 念のため古い設定を残す.
+;; (when (require 'highlight-symbol nil t)
+;;   (with-eval-after-load 'highlight-symbol
+;;     (global-set-key (kbd "C-3") 'highlight-symbol-at-point)
+;;     (global-set-key (kbd "M-C-3") 'highlight-symbol-remove-all))
+
+;; (defun my/highlight-symbol-mode-hook ()
+;; ;  (set-variable highlight-symbol-colors '("DarkOrange" "DodgerBlue4" "DeepPink1"))
+;;   (setq highlight-symbol-idle-delay 1.0) ; ハイライトするまでに1.0秒のディレイを入れる.
+;;   )
+
+;; (add-hook 'highlight-symbol-mode-hook 'my/highlight-symbol-mode-hook)
 
