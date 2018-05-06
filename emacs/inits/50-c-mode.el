5a3725836735ee0079f678b7330af5b67da5a7b0
boiled-mag <itaooooo@gmail.com>
2018-05-06 21:20:45 +0900

    C/C++モードの設定見直し.

--- emacs/inits/50-c-mode.el
+++ emacs/inits/50-c-mode.el
@@ -1,40 +1,61 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-;;;
-;;; C++言語向け
-;;;
-(add-hook 'c++-mode-hook
-          (lambda ()
-            (c-set-style "stroustrup")
-            (setq tab-width 4)
-            (setq c-basic-offset 4)
-            (setq indent-tabs-mode nil)
-            (setq c-hungry-delete-key t)     ; 複数の空白を一度に削除.
-            (c-set-offset 'innamespace 0)    ; namespace {}ではインデントしない.
-            (c-set-offset 'arglist-intro '+) ; 引数リストの引数の一行目.
-            (c-set-offset 'inher-intro '+)   ;
-            (c-set-offset 'case-label '+)    ; switch文内のcase/default label
-            ))
+;;
+;; C++言語向け
+;;
+
+;; *.hファイルはc++-modeで開く.
+(setq auto-mode-alist
+      (append '(("\\.h$" . c++-mode))
+              auto-mode-alist))
+
+;; C++モードの個人設定を行う.
+(defun my/c++-mode-hook ()
+  (c-set-style "stroustrup")       ; コードフォーマットのテンプレートしてstroustrupを使用する.
+  (setq indent-tabs-mode nil)      ; インデントではタブを使用せず半角スペースを使用する.
+  (setq c-basic-offset 4)          ; 4文字に設定.
+  (setq tab-width 4)               ; タブ幅を4文字に設定.
+  (setq c-hungry-delete-key t)     ; 複数の空白を一度に削除.
+  (c-set-offset 'innamespace 0)    ; namespace {}ではインデントしない.
+  (c-set-offset 'arglist-intro '+) ; 引数リストの引数の一行目.
+  (c-set-offset 'inher-intro '+)   ;
+  (c-set-offset 'case-label '+)    ; switch文内のcase/default label
+  )
+
+;; C++モードに個人設定関数をフックする.
+(add-hook 'c++-mode-hook 'my/c++-mode-hook)
 
 
-;;;
-;;; C言語向け
-;;;
-;; (add-hook 'c-mode-hook
-;;           '(lambda ()
-;;              (setq c-basic-offset 4)
-;;              (setq c-hungry-delete-key t)     ; 複数の空白を一度に削除.
-;;              ))
+;;
+;; C言語向け
+;;
 
-;;;
-;;; helm-gtags-mode
-;;;
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
+;; Cモードの個人設定を行う関数.
+(defun my/c-mode-hook ()
+  (c-set-style "stroustrup")       ; コードフォーマットのテンプレートしてstroustrupを使用する.
+  (setq indent-tabs-mode nil)      ; インデントではタブを使用せず半角スペースを使用する.
+  (setq c-basic-offset 4)          ; 4文字に設定.
+  (setq tab-width 4)               ; タブ幅を4文字に設定.
+  (setq c-hungry-delete-key t)     ; 複数の空白を一度に削除.
+  (c-set-offset 'arglist-intro '+) ; 引数リストの引数の一行目.
+  (c-set-offset 'case-label '+)    ; switch文内のcase/default label
+  )
 
-;;;
-;;; highlight-symbol-mode
-;;;
+;; Cモードに個人設定関数をフックする.
+(add-hook 'c-mode-hook 'my/c-mode-hook)
+
+
+;;
+;; helm-gtags-mode
+;;
+;; helm-modeからivy-modeを使用するように変更したため, これらの設定は無効にする.
+;;(add-hook 'c-mode-hook 'helm-gtags-mode)
+;;(add-hook 'c++-mode-hook 'helm-gtags-mode)
+
+
+;;
+;; highlight-symbol-mode
+;;
 (add-hook 'c-mode-hook 'highlight-symbol-mode)
 (add-hook 'c++-mode-hook 'highlight-symbol-mode)
 
