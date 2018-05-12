a8ef5333f5d6c99da874061ac33bc7c6a7333ef7
boiled-mag <itaooooo@gmail.com>
2018-05-12 22:14:23 +0900

    ・MSYS2(MinGW64)のパスを環境変数に追加. ・irony-modeの設定を追加.

--- emacs/inits/00-execpath.el
+++ emacs/inits/00-execpath.el
@@ -2,7 +2,8 @@
 
 (setenv "PATH"
         (concat
-         "C:\\cygwin64\\bin;"
+         "C:\\cygwin64\\bin" ";"
+         "C:\\msys64\\mingw64\\bin" ";"
          (getenv "PATH")))
 
-(setq exec-path (append exec-path '("C:\\cygwin64\\bin")))
+(setq exec-path (append exec-path '("C:\\cygwin64\\bin" "c:/msys64/mingw64/bin")))
