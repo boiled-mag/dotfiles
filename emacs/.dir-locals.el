a6cd30028b07ba65503272beffc4100c85e7b8e7
boiled-mag <itaooooo@gmail.com>
2018-05-23 22:48:27 +0900

    ・C/C++でプロジェクト(ディレクトリ)ごとの設定を行うためのファイルを登録. プロジェクトルートに格納する.

---
+++ emacs/.dir-locals.el
@@ -0,0 +1,8 @@
+;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+
+((nil . ((flycheck-clang-language-standard . "c++11")
+         (flycheck-clang-include-path . ("."
+                                         "src"
+                                         ))
+         )
+      ))
